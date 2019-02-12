4607fe1ef3ab30ac45766a26a3e774bdf51907e0
geetha1998 <43902304+geetha1998@users.noreply.github.com>
2019-02-12 14:25:58 +0530

    Add files via upload

---
+++ Call.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Call
+{
+    public static void main(String args[])
+    {
+        
+            Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
+            for(int i=0;i<n;i++)
+            {
+            System.out.println("hello");
+        }
+    }
+}
