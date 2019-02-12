31d5a77320a20acf7edd7255860f865f0dbe1e39
geetha1998 <43902304+geetha1998@users.noreply.github.com>
2019-02-12 12:18:19 +0530

    Add files via upload

--- Alphabet.java
+++ Alphabet.java
@@ -3,7 +3,7 @@ public class Alphabet
     public static void main(String args[])
     
     {
-        char c='*';
+        char c='a';
         if((c>='a'&& c>='z')||(c>='A'&& c>='Z'))
     System.out.println(c +"is an alphabet");
         else
