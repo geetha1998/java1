8a3d4a2b296a9918b761121db698e711afa49eb1
geetha1998 <43902304+geetha1998@users.noreply.github.com>
2019-02-12 12:32:15 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Compare
+{
+    public static void main (String args[])
+    {
+        int a=7,b=4,c=1;
+        if(a>b)
+        {
+            
+            
+            System.out.println("a is greater");
+            }
+            else if(b>c)
+            {
+                System.out.println("b is greater");
+            }
+                else
+                
+                {
+                    System.out.println("c is greater");
+                }
+            
+        
+    }
+}
